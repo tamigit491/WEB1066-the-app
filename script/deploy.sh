6f88e5c370a67aebb525b74b0465589d5f1d7cbe
tamigit491 <67412733+tamigit491@users.noreply.github.com>
2020-06-25 15:24:41 +0200

    Revert "step 10"

--- script/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-bash -c "echo \"${DOCKER_PASSWORD}\" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin ;"
-docker pull zutherb/monolithic-shop
-docker tag zutherb/monolithic-shop:latest ${DOCKER_LOGIN}/monolithic:latest
-docker push ${DOCKER_LOGIN}/monolithic:latest
